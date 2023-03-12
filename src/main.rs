e1b3c427ffbde2c498913b4275760616da003979
KPMGE <jesuskevin254@gmail.com>
2023-03-11 21:10:47 -0300

    :sparkles: accept more than one message

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 use tokio::{net::TcpListener, io::{AsyncReadExt, AsyncWriteExt}};
 
 #[tokio::main]
@@ -6,13 +5,17 @@ async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
     let (mut socket, _) = listener.accept().await.unwrap();
-    let mut buffer = [0u8; 1024];
-    let bytes_read = socket.read(&mut buffer).await.unwrap();
 
-    println!("messge received: ");
-    for c in 0..bytes_read {
-        print!("{}", buffer[c] as char);
+
+    loop {
+        let mut buffer = [0u8; 1024];
+        let bytes_read = socket.read(&mut buffer).await.unwrap();
+
+        println!("messge received: ");
+        for c in 0..bytes_read {
+            print!("{}", buffer[c] as char);
+        }
+
+        socket.write_all(&buffer[..bytes_read]).await.unwrap();
     }
-
-    socket.write_all(&buffer[..bytes_read]).await.unwrap();
 }

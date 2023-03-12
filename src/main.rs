7770f841d76e5ba97e07cd5da4d5dae85915a895
KPMGE <jesuskevin254@gmail.com>
2023-03-11 22:31:44 -0300

    :sparkles: add support for multiple clients

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,27 @@ use tokio::{
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    let (mut socket, _) = listener.accept().await.unwrap();
-
-    let (reader, mut writer) = socket.split();
-
-    let mut buf_reader = BufReader::new(reader);
-    let mut line = String::new();
-
     loop {
-        let bytes_read = buf_reader.read_line(&mut line).await.unwrap();
+        let (mut socket, _) = listener.accept().await.unwrap();
 
-        // client disconnected
-        if bytes_read == 0 {
-            break;
-        }
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
+            let mut buf_reader = BufReader::new(reader);
+            let mut line = String::new();
 
-        println!("messge: {}", line);
+            loop {
+                let bytes_read = buf_reader.read_line(&mut line).await.unwrap();
 
-        writer.write_all(line.as_bytes()).await.unwrap();
-        line.clear();
+                // client disconnected
+                if bytes_read == 0 {
+                    break;
+                }
+
+                println!("messge: {}", line);
+
+                writer.write_all(line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
     }
 }
